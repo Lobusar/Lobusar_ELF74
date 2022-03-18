c1de95b5822828081e99baa7f2d4f6b3d3ff8738
Bruno Guillen <bruno.guillen@gmail.com>
2022-03-17 21:10:47 -0300

    Update helloword.cpp
    xD

--- Lab1/helloword.cpp
+++ Lab1/helloword.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    float x =0.0;
+    std::cout << "Hello World! \n";
+    std::cout << __DATE__ ,"\n";
+    std::cout <<__TIME__ , "\n";
+    std::cout <<"Input x:\n";
+    std::cin >> x;
+    x=x+1.5; //sum x+1.5
+    std::cout<<"Result: \n";
+    std::cout<<x;
     return 0;
 }
